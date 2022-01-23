a8c1f812c436f4d513c2a069320fe1ae9c083e9a
ujangaja <ujangaja@gmail.com>
2022-01-23 20:05:42 +0700

    About, Contact, and Compose

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('home', { startingContent: homeStartingContent });
 });
+app.get('/about', (req, res) => {
+  res.render('about', { aboutContent: aboutContent });
+});
+app.get('/contact', (req, res) => {
+  res.render('contact', { contactContent: contactContent });
+});
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
 
 app.listen(3000, function () {
   console.log('Server started on port 3000');
--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 ## Home Page
 
 pada browser: http://localhost:3000/
+
+## About, Contact, and Compose
+
+pada browser: - http://localhost:3000/about - http://localhost:3000/contact - http://localhost:3000/compose
